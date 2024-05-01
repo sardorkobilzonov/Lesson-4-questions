d0b74e1139239741a61e77a0e9ae6a959aa960f6
sardorkobilzonov <sardorkobilzonov4@gmail.com>
2024-05-01 22:22:40 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,56 @@
+// 1-SAVOL
+// console.log("" + 1 + 0); javob-10
+// console.log("" - 1 + 0); javob- -1
+// console.log(true + false); // javob-1
+// console.log(6 / "3"); // javob-2;
+// console.log("2" * "3"); // javob-6;
+// console.log(4 + 5 + "px"); // javob-9px;
+// console.log("$" + 4 + 5); // javob-$45;
+// console.log("4" - 2); // javob-2;
+// console.log("4px" - 2); // javob-NaN;
+// console.log("  -9  " + 5); // javob- -9 5;
+// console.log("  -9  " - 5); // javob- -14;
+// console.log(null + 1); // javob- 1;
+// console.log(undefined + 1); // javob- NaN;
+// console.log(" \t \n" - 2); // javob- -2;
+
+// 2 - SAVOL
+// console.log(5 > 4); // javob- true
+// console.log("apple" > "pineapple"); // javob- false
+// console.log("2" > "12"); // javob- true
+// console.log(undefined == null); // javob- true
+// console.log(undefined === null); // javob- false
+// console.log(null == "\n0\n"); // javob- false
+// console.log(null === +"\n0\n"); // javob- false
+
+// 3-SAVOL
+
+// if ("0") {
+//   alert("Hello");
+// } // javob- ha ALERT ishlaydi
+
+// 4- SAVOL
+// let number;
+// let num = prompt("Bitta sonni tanlang?");
+// if (num == 1) {
+//   console.log("0 dan katta");
+// } else if (num == -1) {
+//   console.log("0 dan kichik");
+// } else if (num == 0) {
+//   console.log("0 ga teng");
+// }
+
+// console.log(number);
+
+// 5-SAVOL
+// let result = a + b < 4 ? "Below" : "Over";
+
+// 6-SAVOL
+// let message =
+//   login == "Employee"
+//     ? "Hello"
+//     : login == "Director"
+//     ? "Greetings"
+//     : login == ""
+//     ? "No login"
+//     : "";
